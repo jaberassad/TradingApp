33f013d34f2e8f7d48e57f11670a9fccedea1a1e
y12806ja <bachar.assad@student.manchester.ac.uk>
2025-09-01 11:38:04 +0100

    db created on localhost if not created already

--- trading-app-backend/src/test/java/com/assadosman/Trading/App/controllers/TransactionControllerIntegrationTest.java
+++ trading-app-backend/src/test/java/com/assadosman/Trading/App/controllers/TransactionControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import com.assadosman.Trading.App.model.Transactions.Transaction;
 import com.assadosman.Trading.App.model.user.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +25,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("test")
 public class TransactionControllerIntegrationTest {
 
     @Autowired
@@ -35,6 +38,18 @@ public class TransactionControllerIntegrationTest {
         this.mockMvc = mockMvc;
         this.objectMapper = new ObjectMapper();
         this.objectMapper.registerModule(new JavaTimeModule()); // Register the module here
+    }
+
+    @BeforeEach
+    void cleanDatabase() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/assets/AAPL")
+                .contentType(MediaType.APPLICATION_JSON));
+        User user = testDataUtil.createUserA();
+        String userJson = objectMapper.writeValueAsString(user);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/user/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson)
+        );
     }
 
     @Test
